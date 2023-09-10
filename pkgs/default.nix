adfc916733316c4316ec7c9250bc02e3bd4a08ac
Linnnus <linnnus@users.noreply.github.com>
2023-09-10 19:52:36 +0200

    Fix smu compilation when stdenv.cc == clang

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,7 @@ pkgs:
   still-awake = pkgs.callPackage ./still-awake { };
 
   # Use patched version from Karl.
-  smu = (pkgs.smu.overrideAttrs (old: {
+  smu = pkgs.smu.overrideAttrs (old: {
     version = "2022-08-01";
     src = pkgs.fetchFromGitHub {
       owner = "karlb";
@@ -14,8 +14,8 @@ pkgs:
       rev = "bd03c5944b7146d07a88b58a2dd0d264836e3322";
       hash = "sha256-Jx7lJ9UTHAOCgPxF2p7ZoZBZ476bLXN5dI0vspusmGo=";
     };
-    hardeningDisable = [ "fortify" ];
-  })).overrideDerivation(old: {
-    NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + "  -Wno-maybe-uninitialized";
+    #hardeningDisable = [ "fortify" ];
+    env.NIX_CFLAGS_COMPILE = (old.env.NIX_CFLAGS_COMPILE or "") + 
+                             (pkgs.lib.strings.optionalString (pkgs.stdenv.cc.isGNU or false) " -Wno-maybe-uninitialized");
   });
 }
