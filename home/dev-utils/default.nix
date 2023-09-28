6a293c778fee063877f5d38ab186f15779754e28
Linnnus <linnnus@users.noreply.github.com>
2023-09-28 09:29:31 +0200

    Add xcode manual pages to MANPATH

--- home/dev-utils/default.nix
+++ home/dev-utils/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home.packages =
@@ -10,4 +10,6 @@
       rlwrap
       tcl-8_6
     ];
+
+    home.sessionVariables.MANPATH = lib.mkIf pkgs.stdenv.isDarwin "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/share/man:/Applications/Xcode.app/Contents/Developer/usr/share/man:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man:$MANPATH";
 }
