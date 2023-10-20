989c8b5415a2564495779207af60c5f1e9b5efca
Linnnus <linnnus@users.noreply.github.com>
2023-10-20 17:06:55 +0200

    Add ~/.local/bin to path

--- home/dev-utils/default.nix
+++ home/dev-utils/default.nix
@@ -20,5 +20,9 @@
     ]
     ++ lib.optional pkgs.stdenv.isDarwin trash;
 
+  # Add system manual pages to the search path on Darwin.
   home.sessionVariables.MANPATH = lib.optionalString pkgs.stdenv.isDarwin "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/share/man:/Applications/Xcode.app/Contents/Developer/usr/share/man:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/share/man:$MANPATH";
+
+  # Add local executables/scripts to path.
+  home.sessionVariables.PATH = "$HOME/.local/bin:$PATH";
 }
