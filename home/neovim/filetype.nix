4c017d6fdc6e991b60ff4fba1d5d6b7da1d5bd69
Linnnus <linnnus@users.noreply.github.com>
2023-10-22 11:33:23 +0200

    Add janet filetype

--- home/neovim/filetype.nix
+++ home/neovim/filetype.nix
@@ -8,9 +8,24 @@
       hash = "sha256-c5eUZiKIjAfjJ33l821h5DjozMpMf0CaK03QIkSUfxg=";
     };
   };
+  vim-janet = pkgs.vimUtils.buildVimPlugin {
+    pname = "janet.vim";
+    version = "02-07-2023"; # day of commit
+
+    src = pkgs.fetchFromGitHub {
+      owner = "janet-lang";
+      repo = "janet.vim";
+      rev = "dc14b02f2820bc2aca777a1eeec48627ae6555bf";
+      hash = "sha256-FbwatEyvvB4VY5fIF+HgRqFdeuEQI2ceb2MrZAL/HlA=";
+    };
+
+    nativeBuildInputs = [pkgs.janet];
+  };
 in {
   programs.neovim.plugins = with pkgs.vimPlugins; [
+    # Filetype plugins
     vim-nix
     vim-noweb
+    vim-janet
   ];
 }
