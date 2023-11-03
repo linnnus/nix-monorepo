0338242491abf568c75247efcd6d3afb7faa334e
Linnnus <linnnus@users.noreply.github.com>
2023-11-03 10:03:24 +0100

    all: Update (flake inputs + vim-noweb)

--- pkgs/vim-noweb/default.nix
+++ pkgs/vim-noweb/default.nix
@@ -7,6 +7,6 @@ vimUtils.buildVimPlugin {
   version = "26-08-2023"; # day of retrieval
   src = fetchzip {
     url = "https://metaed.com/papers/vim-noweb/vim-noweb.tgz";
-    hash = "sha256-c5eUZiKIjAfjJ33l821h5DjozMpMf0CaK03QIkSUfxg=";
+    hash = "sha256-kZnbbRM6h1xVRb5Dp+QcmnvD4k0gCDIe7lMDdQRbBMg";
   };
 }
