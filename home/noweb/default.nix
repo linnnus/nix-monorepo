b34e9ee5ab32e434c4c6e15ba8a76361d66f2c98
Linnnus <linnnus@users.noreply.github.com>
2023-11-10 08:51:15 +0100

    home/noweb: mfw spelling

--- home/noweb/default.nix
+++ home/noweb/default.nix
@@ -3,6 +3,6 @@
 
   # Prepend nowebs STY files to the search path. I chose to do it globally,
   # rather than using `makeWrapper` because I sometimes want to manually invoke
-  # `pdflatex` and the like on the output of `nowave`.
+  # `pdflatex` and the like on the output of `noweave`.
   home.sessionVariables.TEXINPUTS = "${pkgs.noweb.tex}/tex/latex/noweb/:$TEXINPUTS";
 }
