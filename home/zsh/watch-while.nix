96fbb3074d44eb66f8ea31e2c9ba65264f6ad346
Linnnus <linnnus@users.noreply.github.com>
2023-11-20 19:43:51 +0100

    home/watch-while: Add ww alias

--- home/zsh/watch-while.nix
+++ home/zsh/watch-while.nix
@@ -7,13 +7,19 @@
   # Program to invoke for long-running commands.
   pkg = pkgs.watch-while;
   exec = "${pkg}/bin/watch-while";
+
+  # Prorams to wrap with watch-while.
+  toWrap = ["nixos-rebuild" "darwin-rebuild"];
 in {
   # Alias long-running commands to their prefixed versions. These aliases are
   # only loaded for interactive use, so they won't mess with scripts.
   programs.zsh.shellAliases =
-    lib.genAttrs ["nixos-rebuild" "darwin-rebuild"] (p: "${exec} ${p}")
+    lib.genAttrs toWrap (p: "${exec} ${p}")
     # Enable alias expansion after sudo with this trick.
-    // {"sudo" = "sudo ";};
+    // {
+      "sudo" = "sudo ";
+      "ww" = "watch-while ";
+    };
 
   # Also add the program to the environment for manual invocation.
   home.packages = [pkg];
