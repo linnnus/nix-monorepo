0b4080777d9cd1bceb89a41b6acf4234420ff1c4
Linnnus <linnnus@users.noreply.github.com>
2023-12-29 21:41:00 +0100

    hosts/ahmed: Bump push-notification-api

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     };
     push-notification-api = {
       url = "github:linnnus/push-notification-api";
-      # url = "path:/home/linus/code/push-notification-api";
+      # url = "path:/home/linus/push-notification-api";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     comma = {
