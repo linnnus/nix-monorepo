01fe87cd98f3acce47682f161d7c391638c2a38d
Linnnus <linnnus@users.noreply.github.com>
2024-01-20 16:43:23 +0100

    formating smh...

--- modules/nixos/hellohtml.linus.onl/default.nix
+++ modules/nixos/hellohtml.linus.onl/default.nix
@@ -32,27 +32,27 @@ in {
         # NOTE: These settings work in tanden with Cloudflare Proxy settings descibed here:
         #       https://blog.devops.dev/implementing-server-sent-events-with-fastapi-nginx-and-cloudflare-10ede1dffc18
         extraConfig = ''
-            location /listen/ {
-              # Have to duplicate this here, as this directive is not inherited.
-              # See: https://blog.martinfjordvald.com/understanding-the-nginx-configuration-inheritance-model/
-              # See: https://serverfault.com/q/1082562
-              proxy_pass ${proxyPass};
-              # Disable connection header.
-              # See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
-              # See: https://www.nginx.com/blog/avoiding-top-10-nginx-configuration-mistakes/#no-keepalives
-              proxy_set_header Connection \'\';
-              # Disable buffering. This is crucial for SSE to ensure that
-              # messages are sent immediately without waiting for a buffer to
-              # fill.
-              proxy_buffering off;
-              # Disable caching to ensure that all messages are sent and received
-              # in real-time without being cached by the proxy.
-              proxy_cache off;
-              # Set a long timeout for reading from the proxy to prevent the
-              # connection from timing out. You may need to adjust this value
-              # based on your specific requirements.
-              proxy_read_timeout 86400;
-            }
+          location /listen/ {
+            # Have to duplicate this here, as this directive is not inherited.
+            # See: https://blog.martinfjordvald.com/understanding-the-nginx-configuration-inheritance-model/
+            # See: https://serverfault.com/q/1082562
+            proxy_pass ${proxyPass};
+            # Disable connection header.
+            # See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
+            # See: https://www.nginx.com/blog/avoiding-top-10-nginx-configuration-mistakes/#no-keepalives
+            proxy_set_header Connection \'\';
+            # Disable buffering. This is crucial for SSE to ensure that
+            # messages are sent immediately without waiting for a buffer to
+            # fill.
+            proxy_buffering off;
+            # Disable caching to ensure that all messages are sent and received
+            # in real-time without being cached by the proxy.
+            proxy_cache off;
+            # Set a long timeout for reading from the proxy to prevent the
+            # connection from timing out. You may need to adjust this value
+            # based on your specific requirements.
+            proxy_read_timeout 86400;
+          }
         '';
       };
     };
