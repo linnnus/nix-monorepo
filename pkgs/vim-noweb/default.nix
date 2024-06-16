c8b64a7f95bb3e6074fb36127b843e7879fbd8c4
Linnnus <linnnus@users.noreply.github.com>
2024-06-16 22:35:20 +0200

    Bump vim-noweb

--- pkgs/vim-noweb/default.nix
+++ pkgs/vim-noweb/default.nix
@@ -4,9 +4,9 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "vim-noweb";
-  version = "26-08-2023"; # day of retrieval
+  version = "23-03-2024"; # modification date on server
   src = fetchzip {
     url = "https://metaed.com/papers/vim-noweb/vim-noweb.tgz";
-    hash = "sha256-kZnbbRM6h1xVRb5Dp+QcmnvD4k0gCDIe7lMDdQRbBMg";
+    hash = "sha256-ejNaEUFHj6xYc30pmgnVVAXaTUQTC5JCwSxxeSanC5k=";
   };
 }
