a2f2f142a64ea34637e1586728d6e4e33553040f
Linnnus <linnnus@users.noreply.github.com>
2024-11-28 11:29:36 +0100

    pkgs/cscript: Use unstable LLVM

--- pkgs/cscript/default.nix
+++ pkgs/cscript/default.nix
@@ -2,7 +2,9 @@
   stdenv,
   lib,
   fetchFromGitHub,
-  llvmPackages,
+  # TODO: I think the PR will become part of 24.11, at which point this becomes unnecessary.
+  # TEMP: We unstable for NixOS/nixpkgs#309165 for LLDB to work on Darwin.
+  unstable,
 }: let
   self = stdenv.mkDerivation rec {
     pname = "cscript";
@@ -18,8 +20,8 @@
     # Instead of using the system CC and LLDB (impure), use the most recent LLVM release.
     postPatch = let
       toStringLiteral = lib.flip lib.pipe [builtins.toJSON lib.strings.escapeShellArg];
-      ccPathLiteral = toStringLiteral "${llvmPackages.clang}/bin/clang";
-      lldbPathLiteral = toStringLiteral "${llvmPackages.lldb}/bin/lldb";
+      ccPathLiteral = toStringLiteral "${unstable.llvmPackages.clang}/bin/clang";
+      lldbPathLiteral = toStringLiteral "${unstable.llvmPackages.lldb}/bin/lldb";
     in ''
       substituteInPlace cscript.c \
         --replace-fail '"cc"' ${ccPathLiteral} \
