143ae9c5285815b82d5d192c505a842512ba4322
Linnnus <linnnus@users.noreply.github.com>
2024-12-29 15:29:49 +0000

    hosts/omar: Show battery and clock in DWM bar

--- hosts/omar/desktop-environment/default.nix
+++ hosts/omar/desktop-environment/default.nix
@@ -8,6 +8,21 @@
 
   services.xserver.windowManager.dwm.enable = true;
 
+  systemd.services.dwm-battery = {
+    description = "Battery status update";
+    after = ["graphical-session.target"];
+    wantedBy = ["graphical-session.target"];
+
+    serviceConfig = {
+      script = ''
+        while true; do
+          echo "$(date +%H:%M) - $(cat /sys/class/power_supply/BAT0/capacity)"
+          sleep 5
+        done | ${pkgs.dwm-setstatus}/bin/dwm-setstatus
+      '';
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     st
     dmenu
