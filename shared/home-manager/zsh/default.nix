1dfd7f00aeae61ed3b2708166a50ba25c5a13b00
Linnnus <linnnus@users.noreply.github.com>
2025-01-18 13:51:56 +0100

    Include Git branch in ZSH prompt

--- shared/home-manager/zsh/default.nix
+++ shared/home-manager/zsh/default.nix
@@ -18,8 +18,14 @@
         # smol prompt
         PROMPT='%# '
       else
+        function _prompt_git_branch_name() {
+          local branch="$(git symbolic-ref HEAD 2>/dev/null | awk 'BEGIN{FS="/"} {print $NF}')"
+          if ! [ -z "$branch" ]; then
+            echo ' ('$branch')'
+          fi
+        }
         # loong looooong prooooompt – Nagāi Sakeru Gumi
-        PROMPT='%B%(2L.LVL%L .)%b%F{red}%(?..E%? )%f%F{93}%n%f@%F{35}%m%f%# '
+        PROMPT='%B%(2L.LVL%L .)%b%F{red}%(?..E%? )%f%F{93}%n%f@%F{35}%m%f%F{blue}$(_prompt_git_branch_name)%f# '
       fi
       RPROMPT='%F{green}%$((COLUMNS/4))<...<%~%<<%f'
 
