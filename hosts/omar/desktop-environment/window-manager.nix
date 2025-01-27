bbf6d817a960b2188829adcd99189423598ba6fe
Linnnus <linnnus@users.noreply.github.com>
2025-01-27 19:48:16 +0000

    hosts/omar: Show battery icons in DWM status bar

--- hosts/omar/desktop-environment/window-manager.nix
+++ hosts/omar/desktop-environment/window-manager.nix
@@ -12,7 +12,21 @@
 
     serviceConfig.ExecStart = pkgs.writeShellScript "dwm-battery" ''
         while true; do
-          echo "$(date +%H:%M) - $(cat /sys/class/power_supply/BAT0/capacity)%"
+          echo -n "$(date +%H:%M) - ";
+
+          # See: https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-power
+          case "$(cat /sys/class/power_supply/BAT0/status)" in
+            Charging)       echo -n "🔋 " ;;
+            Discharging)    echo -n "🪫 " ;;
+            "Not charging") echo -n "🪫 " ;;
+            Full)           echo -n "🔋 " ;;
+            Unknown)        echo -n "? " ;;
+            ""|*)           echo -n "?? " ;;
+          esac
+
+          echo -n "$(cat /sys/class/power_supply/BAT0/capacity)%"
+
+          echo
           sleep 5
         done | ${pkgs.dwm-setstatus}/bin/dwm-setstatus
       '';
