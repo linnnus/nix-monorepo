645ec7c7a5069a246a53e46b16d99b091d6b7328
Linnnus <linnnus@users.noreply.github.com>
2025-02-08 16:28:37 +0100

    muhammed: Add mcping to path

--- hosts/muhammed/extra-utils.nix
+++ hosts/muhammed/extra-utils.nix
@@ -4,6 +4,23 @@
   xkcdpass' = pkgs.writeShellScriptBin "xkcdpass" ''
     ${pkgs.xkcdpass}/bin/xkcdpass --delimiter="" --case capitalize --numwords=5 "$@"
   '';
+
+  mcinfo = let
+    jq-script = pkgs.writeText "minecraft.jq" ''
+        .version as $v
+      | .description as $d
+      | .players as $p
+      | (if env.NO_COLOR == null then "\u001b[1m" else "" end) as $bold
+      | (if env.NO_COLOR == null then "\u001b[0m" else "" end) as $reset
+      | "\($bold)Version:\($reset) \($v.name) (\($v.protocol))",
+        "\($bold)Description:\($reset) \($d)",
+        "\($bold)Players online\($reset): \($p.online)"
+    '';
+  in
+    pkgs.writeShellScriptBin "mcinfo" ''
+      set -x -u -o pipefail
+      ${pkgs.mcping}/bin/mcping "$@" | jq --raw-output --from-file ${jq-script}
+    '';
 in {
   home.packages = with pkgs; [
     imagemagick
@@ -11,5 +28,9 @@ in {
 
     # Generating passwords
     xkcdpass'
+
+    # Quick monitoring of Minecraft servers
+    mcping
+    mcinfo
   ];
 }
