64564e6a43ecb7664eb5556876a84a55cb2de4c1
Linnnus <linnnus@users.noreply.github.com>
2025-07-19 19:20:42 +0200

    muhammed: run 'gh sync' for all update-git-repos

--- hosts/muhammed/update-git-repos/default.nix
+++ hosts/muhammed/update-git-repos/default.nix
@@ -1,12 +1,27 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   launchd.agents."update-git-repos" = {
     serviceConfig = {
       ProgramArguments = let
+        awk-script = ''
+          $1 == "origin" { origin = 1; }
+          $1 == "upstream" { upstream = 1; }
+          END { exit((origin & &upstream) ? 0 : 1) }
+        '';
+
         script = pkgs.writeShellScript "update-git-repos" ''
           printf '\x1b[1m=> Syncing repos\x1b[0m\n'
           (cd ~/Source/nix ; gh repo sync linnnus/nix)
           printf '\x1b[1m=> Fetching repos\x1b[0m\n'
           find ~/Source ~/Projects -name '*.git' -print0 | while read -d $'\0' -r repo; do
+            if git -C "$repo" remote | awk ${lib.escapeShellArg awk-script}; then
+              printf '\x1b[1m=> Syncing %s\x1b[0m\n' "$repo"
+              gh repo sync "linnnus/$(basename "$repo")"
+            fi
+
             printf '\x1b[1m=> Updating %s\x1b[0m\n' "$repo"
             git -C "$repo" fetch --all
           done
