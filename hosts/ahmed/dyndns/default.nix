548791edd933b0dbee6c4d0bdf626c5fc4711da4
Linnnus <linnnus@users.noreply.github.com>
2025-07-20 14:45:14 +0200

    Update cloudflare-dyndns key format
    With NixOS/nixpkgs@9c02a1e422c4b the way to pass the api token changed.
    
    I moved the huge comment from the secret file into the Nix module.

--- hosts/ahmed/dyndns/default.nix
+++ hosts/ahmed/dyndns/default.nix
@@ -9,7 +9,19 @@
     proxied = true;
   };
 
-  # We have to authenticate the Cloudflare's DDNS service with an API key.
+  # We have to authenticate the Cloudflare's DDNS service with an API key. This
+  # file should contain an API token which should have permission to edit the
+  # DNS records all the domains used in hosts/ahmed/.
+  #
+  # WARNING: This file should NOT be shared among different machines. The token
+  # should ONLY have permission to edit the domains associated with Ahmed.
+  # Anything else would be a violation of the principle of least privelege. A
+  # restructure would be needed in such a case.
+  #
+  # The token belongs to your personal account [0]. See the CloudFlare documentation [1].
+  #
+  # [0]: https://dash.cloudflare.com/profile/api-tokens
+  # [1]: https://developers.cloudflare.com/fundamentals/api/get-started/create-token/
   age.secrets.cloudflare-dyndns-api-token.file = ../../../secrets/cloudflare-ddns-token.env.age;
 
   # We also have to overwrite the dependencies of the DYNDNS client service to
