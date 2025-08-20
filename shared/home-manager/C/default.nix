e4b5e09831d911e3b900e1ac7ad68706a141d5e5
Linnnus <linnnus@users.noreply.github.com>
2025-08-20 17:59:18 +0200

    all: Use llvmPackages_21 for development

--- shared/home-manager/C/default.nix
+++ shared/home-manager/C/default.nix
@@ -1,11 +1,13 @@
 # This module configures development tools for C.
 {pkgs, ...}: let
   isLinux = pkgs.stdenv.isLinux;
+
+  llvmPackages' = pkgs.llvmPackages_21;
 in {
   home.packages = with pkgs;
     [
-      clang
-      clang-manpages
+      llvmPackages'.clang
+      llvmPackages'.clang-manpages
       man-pages-posix
       cscript
     ]
@@ -15,7 +17,7 @@ in {
 
   programs.neovim.extraLuaConfig = ''
     require("lspconfig")["clangd"].setup({
-      cmd = { "${pkgs.clang-tools}/bin/clangd", "--background-index", "--clang-tidy" },
+      cmd = { "${llvmPackages'.clang-tools}/bin/clangd", "--background-index", "--clang-tidy" },
       on_attach = function(_, bufnr)
         vim.keymap.set("n", "<leader>s", function()
           vim.cmd [[ClangdSwitchSourceHeader]]
