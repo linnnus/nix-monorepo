fe37cd6de305b0eee92d04b50679945c46a62623
Linnnus <linnnus@users.noreply.github.com>
2026-08-09 18:29:07 +0200

    ahmed: Reject unrecognized requests to NGINX

--- hosts/ahmed/nginx/default.nix
+++ hosts/ahmed/nginx/default.nix
@@ -7,6 +7,37 @@
   # Virtual hosts.
   services.nginx.enable = true;
 
+  # I've had some issues with a random virtual host (depending on evaluation
+  # order, I guess) being promoted to Default Server(TM). It results in weird
+  # behavior when the requested host isn't recognized: like sending the wrong
+  # certificate and stuff. Very annoying.
+  #
+  # This explicit default host should hopefully solve that by just dropping
+  # any requests as soon as possible.
+  services.nginx.virtualHosts = {
+    "default-virtual-host" = {
+      serverName = "_";
+      default = true;
+
+      # For HTTP, the non-standard return code 444 indicates to NGINX that it
+      # should drop the request immediately without sending a response to the
+      # client [0]. This is the recommended way to reject unrecognized server
+      # names [1].
+      #
+      # [0]: https://nginx.org/en/docs/http/ngx_http_rewrite_module.html#return
+      # [1]: https://nginx.org/en/docs/http/request_processing.html#how_to_prevent_undefined_server_names
+      locations."/".return = "444";
+
+      # For HTTPS, we don't want to use "return 444" as the directive is only
+      # processed _after_ NGINX already has established an SSL connection which
+      # means sending over a (nonsense) certificate. The `ssl_reject_handshake`
+      # directive exists explicitly for this purpose [0].
+      #
+      # [0]: https://nginx.org/en/docs/http/ngx_http_ssl_module.html#ssl_reject_handshake
+      rejectSSL = true; # Drop SSL connections before a certificate has been sent.
+    };
+  };
+
   # Configure ACME. This is used by various HTTP services through the NGINX virtual hosts.
   security.acme = {
     acceptTerms = true;
