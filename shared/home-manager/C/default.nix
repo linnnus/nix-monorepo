4cdbaf8f5c8e07d90b549d9a99ad5043f5c61f15
Linnnus <linnnus@users.noreply.github.com>
2026-08-11 12:03:29 +0200

    nvim: Fix call to non-existant command ClangdSwitchSourceHeader
    This was failing because the command is registered by on_attach from
    lsp-config but we were overriding that with our own implementation.

--- shared/home-manager/C/default.nix
+++ shared/home-manager/C/default.nix
@@ -16,11 +16,30 @@ in {
     ];
 
   programs.neovim.initLua = ''
+    -- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+    local function switch_source_header(bufnr, client)
+      local method_name = 'textDocument/switchSourceHeader'
+      if not client or not client:supports_method(method_name) then
+        return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+      end
+      local params = vim.lsp.util.make_text_document_params(bufnr)
+      client:request(method_name, params, function(err, result)
+        if err then
+          error(tostring(err))
+        end
+        if not result then
+          vim.notify('corresponding file cannot be determined')
+          return
+        end
+        vim.cmd.edit(vim.uri_to_fname(result))
+      end, bufnr)
+    end
+
     vim.lsp.config("clangd", {
       cmd = { "${llvmPackages'.clang-tools}/bin/clangd", "--background-index", "--clang-tidy" },
-      on_attach = function(_, bufnr)
+      on_attach = function(client, bufnr)
         vim.keymap.set("n", "<leader>s", function()
-          vim.cmd [[ClangdSwitchSourceHeader]]
+          switch_source_header(bufnr, client)
         end, {
           noremap=true,
           silent=true,
